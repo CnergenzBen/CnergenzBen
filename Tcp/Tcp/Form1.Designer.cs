d8ee7159cd2cdf19e4f551276ebfc45c899e300b
ben <ben@Cnergenz.com>
2021-07-27 10:43:08 +0800

    TCP
    27-7 Tcp upload

---
+++ Tcp/Tcp/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace Tcp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if(disposing && ( components != null ))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent( )
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(376, 128);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(138, 26);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(178, 116);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 67);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Send";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(178, 235);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(111, 67);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(178, 360);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(111, 67);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "connect";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(178, 472);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(111, 67);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Data";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(376, 472);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(111, 67);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Sled";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(426, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(626, 128);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 26);
+            this.textBox2.TabIndex = 7;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(626, 497);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(111, 67);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "Crc";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(376, 206);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(111, 67);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "Send no crc";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1023, 672);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ Tcp/Tcp/Form1.cs
@@ -0,0 +1,364 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+
+namespace Tcp
+{
+   
+
+    public partial class Form1:Form
+    {
+        Tcp.TcpOption t1 = new TcpOption();
+        Tcp.TcpOption.AllResult Result;
+        //声明
+        public Form1( )
+        {
+            InitializeComponent();
+            
+        }
+
+        private void button1_Click(object sender , EventArgs e)
+        {
+           for(int i = 0; i < 10; i++)
+            {
+                 t1.Action = int.Parse(textBox2.Text);
+                 byte [ ] bytes = Encoding.ASCII.GetBytes(textBox1.Text.ToString());
+                Result = t1.TCPSend(bytes);
+                Console.WriteLine("Error = "+Result.ErrorMessage+"  send = " + Result.Result + " data Send = "+ Result.DataSend + " DataResult = " + Result.DataReceive + "  Data byte Receive= " + BitConverter.ToString(Result.ByteDataReceive) );
+                Thread.Sleep(100);
+            }
+          
+        }
+
+        private void button2_Click(object sender , EventArgs e)
+        {
+            t1.TcpClose();
+            Console.WriteLine(t1.TCPResult.Result);
+                        
+        }
+
+        private void button3_Click(object sender , EventArgs e)
+        {
+          t1.ConnectTcp("192.168.1.2" , "2");
+           Console.WriteLine(t1.TCPResult.ErrorMessage +" == "+ t1.TCPResult.Result);
+        }
+
+        private void button4_Click(object sender , EventArgs e)
+        {
+            
+        }
+       
+        private void button5_Click(object sender , EventArgs e)
+        {
+           
+            t1.TCPSend(Encoding.ASCII.GetBytes(textBox1.Text.ToString()));
+            
+        }
+
+        private void button6_Click(object sender , EventArgs e)
+        {
+            byte [ ] test = Encoding.ASCII.GetBytes("PULL234");
+            Console.WriteLine(BitConverter.ToString(test));
+            Console.WriteLine(BitConverter.ToString(t1.ToModbus(test)));
+        }
+
+        private void Form1_Load(object sender , EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender , EventArgs e)
+        {
+            t1.Action = int.Parse(textBox2.Text);
+            byte [ ] bytes = Encoding.ASCII.GetBytes(textBox1.Text.ToString());
+            Result = t1.TCPSendOriginal(bytes);
+            Console.WriteLine("Error = " + Result.ErrorMessage + "  send = " + Result.Result + " data Send = " + Result.DataSend +" DataResult = " + Result.DataReceive+"  Data byte Receive " + BitConverter.ToString(Result.ByteDataReceive));
+            Thread.Sleep(100);
+        }
+    }
+
+
+
+    public class TcpOption
+    {
+
+        public class AllResult
+        {
+            public String DataReceive;
+            public byte[] ByteDataReceive = new byte[2048];
+            public String DataSend;
+            public byte[] ByteDataSend = new byte[2048];
+            public string ErrorMessage;
+            public bool Result;
+            public Byte[] CRC = new byte[10];
+        }
+
+        public int Action;
+        public byte [ ] buffer = new byte [2048];
+        public byte [] bufferRecive;
+
+        private byte [ ] PULL = Encoding.ASCII.GetBytes("PULL");
+        private byte [ ] SENR = Encoding.ASCII.GetBytes("SENR");
+        private byte [] Crc;
+        private byte [ ] buffer3;
+        private Socket socketSend;
+
+        public AllResult TCPResult = new AllResult();
+
+        private bool SocketConnected(Socket s)
+        {
+            bool part1 = s.Poll(1000 , SelectMode.SelectRead);
+            bool part2 = ( s.Available == 0 );
+            if(part1 && part2)
+                return false;
+            else
+                return true;
+        }
+
+        private bool IsSocketConnected(Socket client)
+        {
+            bool blockingState = client.Blocking;
+            try
+            {
+                byte [ ] tmp = new byte [1];
+                client.Blocking = false;
+                client.Send(tmp , 0 , 0);
+                return false;
+            }
+            catch(SocketException e)
+            {
+                // 产生 10035 == WSAEWOULDBLOCK 错误，说明被阻止了，但是还是连接的
+                if(e.NativeErrorCode.Equals(10035))
+                    return false;
+                else
+                    return true;
+            }
+            finally
+            {
+                client.Blocking = blockingState;    // 恢复状态
+            }
+        }
+
+        public AllResult ConnectTcp(String IP , string Port)
+        {
+                 TcpClose();
+            try
+            {
+                TcpClient Tcpclient1 = new TcpClient();
+
+                var result = Tcpclient1.BeginConnect(IP, Convert.ToInt32(Port.Trim()),null,null);
+
+                var success = result.AsyncWaitHandle.WaitOne(TimeSpan.FromSeconds(2));
+                if(!success)
+                {
+                    throw new Exception("Failed to connect.");
+                }
+
+                // we have connected
+
+
+
+                socketSend = new Socket(AddressFamily.InterNetwork , SocketType.Stream , ProtocolType.Tcp);
+                IPAddress ip = IPAddress.Parse(IP.Trim());
+                socketSend.Connect(ip , Convert.ToInt32(Port.Trim()));
+                TCPResult.Result = true;
+                return TCPResult;
+
+            }
+            catch
+            {
+                TCPResult.Result = false;
+                TCPResult.ErrorMessage = "Connection TCP Failed";
+                return TCPResult;
+            }
+        }
+
+        private void DataProcess( byte[]dataSend)
+        {
+            if(Action == 0)
+            {
+                PULL.CopyTo(buffer , 0);
+                dataSend.CopyTo(buffer , PULL.Length);
+                Crc = ToModbus(buffer);
+                TCPResult.CRC = Crc;
+                buffer.CopyTo(buffer3 , 0);
+                Crc.CopyTo(buffer3 , dataSend.Length + PULL.Length);
+            }
+            else if(Action == 1)
+            {
+                SENR.CopyTo(buffer , 0);
+                dataSend.CopyTo(buffer , PULL.Length);
+                Crc = ToModbus(buffer);
+                TCPResult.CRC = Crc;
+                buffer.CopyTo(buffer3 , 0);
+                Crc.CopyTo(buffer3 , dataSend.Length + PULL.Length);
+
+            }
+        }
+
+        public AllResult TCPSend(byte[] dataSend)
+        {
+            buffer = new byte [dataSend.Length + 4];
+            buffer3 = new byte [dataSend.Length + 6];
+            Crc = new byte [2];
+
+
+            DataProcess(dataSend);
+
+            try
+            {
+                try
+                {
+                    TCPResult.ByteDataSend = buffer3;
+                    TCPResult.DataSend     = Encoding.ASCII.GetString(buffer3 , 0 , buffer3.Length);
+                    int receive            = socketSend.Send(buffer3);
+
+                }
+                catch
+                {
+                    TCPResult.Result        = false;
+                    TCPResult.ErrorMessage  = "Send Step Failed";
+                    return TCPResult;
+                }
+
+                try
+                {
+                    TCPResult.ByteDataReceive = new byte [1024];
+                    TCPResult.DataReceive     = "";
+                    int r                     = socketSend.Receive(TCPResult.ByteDataReceive);
+                    TCPResult.DataReceive     = Encoding.ASCII.GetString(TCPResult.ByteDataReceive , 0 , r);
+                }
+                catch
+                {
+                    TCPResult.Result            = false;
+                    TCPResult.ErrorMessage      = "Receive Step Failed";
+                    return TCPResult;
+                }
+                TCPResult.Result = true;
+                TCPResult.ErrorMessage = "Receive Success";
+                return TCPResult;             
+
+            }
+            catch(Exception ex)
+            {
+                return TCPResult;
+                // MessageBox.Show("发送消息出错:" + ex.Message);
+            }
+        }
+
+        public AllResult TCPSendOriginal(byte [ ] dataSend)
+        {
+
+
+            try
+            {
+                try
+                {
+                    TCPResult.ByteDataSend  = dataSend;
+                    TCPResult.DataSend      = Encoding.ASCII.GetString(dataSend , 0 , dataSend.Length);
+                    int receive             = socketSend.Send(dataSend);
+
+                }
+                catch
+                {
+                    TCPResult.Result        = false;
+                    TCPResult.ErrorMessage  = "Send Step Failed";
+                    return TCPResult;
+                }
+
+                try
+                {
+                    TCPResult.ByteDataReceive = new byte [1024];
+                    TCPResult.DataReceive      = "";
+                    int r = socketSend.Receive(TCPResult.ByteDataReceive);
+                    TCPResult.DataReceive = Encoding.ASCII.GetString(TCPResult.ByteDataReceive , 0 , r);
+                }
+                catch
+                {
+                    TCPResult.Result = false;
+                    TCPResult.ErrorMessage = "Receive Step Failed";
+                    return TCPResult;
+                }
+                TCPResult.Result = true;
+                TCPResult.ErrorMessage = "Receive Success";
+                return TCPResult;
+
+            }
+            catch(Exception ex)
+            {
+                return TCPResult;
+                // MessageBox.Show("发送消息出错:" + ex.Message);
+            }
+        }
+
+        public AllResult TcpClose( )
+        {
+            try
+            {
+                if(socketSend != null)
+                {
+                    if(!IsSocketConnected(socketSend))
+                    {
+                        if(SocketConnected(socketSend))
+                        {
+
+                            socketSend.Shutdown(SocketShutdown.Both);
+                            socketSend.Close();
+
+                        }
+                    }
+
+                }
+                TCPResult.Result = true;
+                TCPResult.ErrorMessage = "Sucess Close TCP";
+                return TCPResult;
+            }
+            catch(Exception ex)
+            {
+
+                // MessageBox.Show("Close:" + ex.Message);
+                 TCPResult.Result = false;
+                 TCPResult.ErrorMessage = "Close TCP Failed:" ;
+                 return TCPResult;
+            }
+        }
+
+        public  byte [ ] ToModbus(byte [ ] byteData)
+        {
+            byte [ ] CRC = new byte [2];
+
+            UInt16 wCrc = 0xFFFF;
+            for(int i = 0; i < byteData.Length; i++)
+            {
+                wCrc ^= Convert.ToUInt16(byteData [i]);
+                for(int j = 0; j < 8; j++)
+                {
+                    if(( wCrc & 0x0001 ) == 1)
+                    {
+                        wCrc >>= 1;
+                        wCrc ^= 0xA001;//异或多项式
+                    }
+                    else
+                    {
+                        wCrc >>= 1;
+                    }
+                }
+            }
+
+            CRC [0] = (byte)( ( wCrc & 0xFF00 ) >> 8 );//高位在后
+            CRC [1] = (byte)( wCrc & 0x00FF );       //低位在前
+            return CRC;
+
+        }
+
+    }
+}
